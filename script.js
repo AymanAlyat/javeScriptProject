60d468e808d007d3676a8f48f459d981228379ae
Ayman Alyat <aymanaleliat@gmail.com>
2025-06-17 22:29:54 +0300

    edist js

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 const taskInput = document.getElementById("taskInput");
-
-
 const addTaskBtn = document.getElementById("addTaskBtn");
 
-
 taskInput.addEventListener("input", () => {
-  addTaskBtn.disabled = taskInput.value.trim() === "";//**الهدف امنع المستخدم انه يكبس مدام فش داتا */
+  addTaskBtn.disabled = taskInput.value.trim() === "";/* بمنع المستخدم يكبس او خلينا نقول يمنعو يدخل اذا كان الحقل فاضي */
+}); /*يعني بالمختصر بمنع المستخدم يدخل قيمه فاضيه */
+
+
+
+
+
+
+const inputError = document.getElementById("inputError");
+
+
+//لوجك سيفتي بجوز او اقلك انسا الكومنت اساسا
+
+
+
+addTaskBtn.addEventListener("click", () => {
+  const taskText = taskInput.value.trim(); //اخذ القيمه من المستخدم وحذف الفراغات منها وخزنها
+
+  if (taskText === "") { //بالمختصر المفيد اذا جيت تكبس وانت فاضي بمنعك وبظهرلك رساله الخطا بالمكان اللي تركناه فاضي لرساله الخطا
+    inputError.textContent = "Task cannot be empty."; 
+    return;
+  } else {
+    inputError.textContent = ""; // غير هيك رساله الايرور رح تضل فاضيه
+
+
+    // عملت عنصر عشان اضيف التاسك بيه
+    const taskItem = document.createElement("div");
+    taskItem.textContent = taskText;
+    taskItem.classList.add("todo-item"); 
+    todoList.appendChild(taskItem); //هاي ضروريه عشان اضيف بدونها ما بضيف
+
+
+
+    taskInput.value = "";          // كاني عملت ريست يعني فضيتواا
+    addTaskBtn.disabled = true;    // الكبسه خليناها معطله لانه صار فاضي
+  }
 });
-console.log("Hi in JS");
+
 
